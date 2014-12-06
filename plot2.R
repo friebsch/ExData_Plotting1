b3baebac204ed0f2a982894869790afdb3288d27
friebsch <riebschlaegerf@gmail.com>
2014-12-06 15:44:53 +0100

    final commit

--- plot2.R
+++ plot2.R
@@ -1,23 +1,8 @@
-plot2 <- function() {
-    
-    # Read data
-    hhp <- read.csv('household_power_consumption.txt', sep=';')
-    
-    # Correct date columns
-    hhp$Date <- as.Date(hhp$Date, "%d/%m/%Y")  
-    hhp$Datetime <- paste(hhp$Date, hhp$Time)
-    hhp$Datetime <- strptime(hhp$Datetime, "%Y-%m-%d %H:%M:%S")
-    
-    # Create subset and correct data types
-    sub <- subset(hhp, Date == '2007-02-01' | Date == '2007-02-02')
-    sub$Global_active_power <- as.numeric(as.character(sub$Global_active_power))
-    
-    # Set locale to English to format weekdays correctly (works on Windows only)
-    # Uncomment the following statement if weekdays are generated in your local language
-    #  Sys.setlocale("LC_TIME", "English")
-    
-    # Create plot
-    png('plot2.png')
-    plot(sub$Datetime, sub$Global_active_power, type="line", ylab="Global Active Power (kilowatts)", xlab="")
-    dev.off()
-}
+dataSet <- read.table("household_power_consumption.txt",head = TRUE, sep = ";", na.strings = "?")
+dataSetSub <- subset(dataSet, Date == "1/2/2007" | Date == "2/2/2007")
+dataSetSub$Date <- as.Date(dataSetSub$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(dataSetSub$Date), dataSetSub$Time)
+dataSetSub$Datetime <- as.POSIXct(datetime)
+png(filename = "plot2.png", width = 480, height = 480)
+plot(dataSetSub$Global_active_power~dataSetSub$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
